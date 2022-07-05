cb41574935c05d33724ad4eb946774485fa71070
Gevorg Kuzbashian <75931217+Pacifist-Penguin@users.noreply.github.com>
2022-07-06 00:17:03 +0400

    more graphics settings

--- global_config.gd
+++ global_config.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const PATH = "user://settings.save"
+const PATH = "user://settings.cfg"
 
 const SIMPLE_RESOLUTIONS := {
 	"16:9": ["3840x2160", 
@@ -29,19 +29,40 @@ const SIMPLE_RESOLUTIONS := {
 }
 
 @onready var DEFAULT_SETTINGS := {
-	"resolution": DisplayServer.screen_get_size(),
-	"aspect_ratio": "16:9",
-	"anisotropic_filtration": ProjectSettings.get_setting("rendering/textures/default_filters/anisotropic_filtering_level"),
-	"decals": ProjectSettings.get_setting("rendering/textures/decals/filter"),
-	"light_projectors": ProjectSettings.get_setting("rendering/textures/light_projectors/filter"),
-	"default_texture": ProjectSettings.get_setting("rendering/textures/canvas_textures/default_texture_filter"),
-	
+	"Resolution": DisplayServer.screen_get_size(),
+	"Aspect_ratio": "16:9",
+	"Anisotropic_filtration": ProjectSettings.get_setting("rendering/textures/default_filters/anisotropic_filtering_level"),
+	"Decals": ProjectSettings.get_setting("rendering/textures/decals/filter"),
+	"Light_projectors": ProjectSettings.get_setting("rendering/textures/light_projectors/filter"),
+	"Default_texture": ProjectSettings.get_setting("rendering/textures/canvas_textures/default_texture_filter"),
+	"Thread_model": ProjectSettings.get_setting("rendering/driver/threads/thread_model"),
+	"Half_GI": ProjectSettings.get_setting("rendering/global_illumination/gi/use_half_resolution"),
+	"DOF_Bokeh_Shape": ProjectSettings.get_setting("rendering/camera/depth_of_field/depth_of_field_bokeh_shape"),
+	"DOF_Quality": ProjectSettings.get_setting("rendering/camera/depth_of_field/depth_of_field_bokeh_quality"),
+	"ODF_Jitter": ProjectSettings.get_setting("rendering/camera/depth_of_field/depth_of_field_use_jitter"),
+	"SSAO_Quaity": ProjectSettings.get_setting("rendering/environment/ssao/quality"),
+	"SSIL_Quality": ProjectSettings.get_setting("rendering/environment/ssil/quality"),
+	"Glow_upscale_mode": ProjectSettings.get_setting("rendering/environment/glow/upscale_mode"),
+	"Glow_high_quality": ProjectSettings.get_setting("rendering/environment/glow/use_high_quality"),
+	"Screen_space_reflection_roughness_quaity": ProjectSettings.get_setting("rendering/environment/screen_space_reflection/roughness_quality"),
+	"Subsurface_scattering_quality": ProjectSettings.get_setting("rendering/environment/subsurface_scattering/subsurface_scattering_quality"),
+	"Volumetric_fog_volume_size": ProjectSettings.get_setting("rendering/environment/volumetric_fog/volume_size"),
+	"Volumetric_fog_volume_depth": ProjectSettings.get_setting("rendering/environment/volumetric_fog/volume_depth"),
+	"Volumetric_fog_use_filter": ProjectSettings.get_setting("rendering/environment/volumetric_fog/use_filter"),
+	"Use_TAA": ProjectSettings.get_setting("rendering/anti_aliasing/quality/use_taa"),
+	"Screen_space_AA": ProjectSettings.get_setting("rendering/anti_aliasing/quality/screen_space_aa"),
+	"MSAA": ProjectSettings.get_setting("rendering/anti_aliasing/quality/msaa"),
+	"Use_Debanding": ProjectSettings.get_setting("rendering/anti_aliasing/quality/use_debanding"),
+	"Use_Occlusiton_Culling": ProjectSettings.get_setting("rendering/occlusion_culling/use_occlusion_culling"),
+	"Occlusion_Culling_quality": ProjectSettings.get_setting("rendering/occlusion_culling/bvh_build_quality"),
+	"Mesh_LOD": ProjectSettings.get_setting("rendering/mesh_lod/lod_change/threshold_pixels")
 }
 
 @onready var joy_is_connected : bool = not is_zero_approx(float (Input.get_connected_joypads().size()))
 
+
 func _ready():
-	print("joy is connected:", joy_is_connected)
+	print(DEFAULT_SETTINGS)
 	Input.joy_connection_changed.connect(_on_joy_connection_changed)
 
 func _on_joy_connection_changed(device, dsc):
@@ -51,7 +72,7 @@ func _on_joy_connection_changed(device, dsc):
 var current_settings := {}
 
 func _save_data():
-	var file := File.new()
+	var file := ConfigFile.new()
 	file.open(PATH, File.WRITE)
 	file.store_var(current_settings)
 	file.close()
